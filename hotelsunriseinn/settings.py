28d02efa700e6bc866bf3bf15aa849cf0ea4f90d
Zakaria Fahsi <95761132+pro-gra@users.noreply.github.com>
2025-05-05 21:00:34 +0100

    Update settings.py

--- hotelsunriseinn/settings.py
+++ hotelsunriseinn/settings.py
@@ -12,17 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-2&vtlb%!ikkvep!r!vg&3!d&n)a&5l4smt^4lx35ygvhlgfea_'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -107,8 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
